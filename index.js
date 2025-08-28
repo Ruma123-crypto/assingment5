191ece1adc87d686a11a4c48ac926bec4bb1dfca
Ruma123-crypto <akterrumana940@gmail.com>
2025-08-28 16:36:25 +0600

    clear task done

--- index.js
+++ index.js
@@ -11,25 +11,53 @@ function totalheart(){
 
 // calling function
 
-function calingHotline(text){
+function calingHotline(serviceName,serviceNumber){
 
     const coinMoneyE1=document.getElementById('coin')
     const coinMoney=parseInt(coinMoneyE1.innerText)
     if(coinMoney>=20)
     {
-       alert('📞'+ ' ' +'Calling' +' '+text)
+       alert('📞'+ ' ' +'Calling' +' '+serviceName+' '+serviceNumber)
+        setHistroy(serviceName,serviceNumber)
         const finalMoney=coinMoney-20;
         coinMoneyE1.innerText=finalMoney;
+        
     }
    else{
     alert('❌ আপনার পর্যাপ্ত কয়েন নেই কল করতে কমপক্ষে ২০ টি কয়েন লাগবে')
    }
 }
 
+
+// clear function
+
+function setHistroy(serviceName,serviceNumber)
+{
+const previousElement=document.getElementById('history-content')
+
+    const currentTime=new Date().toLocaleTimeString()
+        previousElement.innerHTML +=` <div class="bg-[#fafafa] rounded-[8px] grid grid-cols-12 gap-3  p-2 m-3 ">
+         <div class="col-span-7">
+          <h3 class="font-semibold text-[11px]"id="railway-text">${serviceName} </h3>
+          <h2 class="text-[#5c5c5c]">${serviceNumber}</h2>
+         
+         </div>
+          <div class="col-span-5  text-[12px]">
+             <span>${currentTime}</span>
+          </div>
+      </div>`
+    
+   
+
+   
+}
+
+
 // heart click code
 
 document.getElementById('national-heart').addEventListener('click',function(){
     totalheart('');
+    
 })
 document.getElementById('police-heart').addEventListener('click',function(){
     totalheart('');
@@ -64,62 +92,69 @@ document.getElementById('national-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('national-text').innerText
     const serviceNumber=document.getElementById('national-service-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+    
+    calingHotline(serviceText,serviceNumber)
+ 
 })
 document.getElementById('police-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('police-service-text').innerText
     const serviceNumber=document.getElementById('police-service-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+    calingHotline(serviceText,serviceNumber)
 })
 document.getElementById('fire-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('fire-service-text').innerText
     const serviceNumber=document.getElementById('fire-service-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+     calingHotline(serviceText,serviceNumber)
+    
 })
 document.getElementById('ambulance-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('ambulance-service-text').innerText
     const serviceNumber=document.getElementById('ambulance-service-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+    calingHotline(serviceText,serviceNumber)
+     
 })
 document.getElementById('Women-child-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('Women-child-text').innerText
     const serviceNumber=document.getElementById('Women-child-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+    calingHotline(serviceText,serviceNumber)
+    
 })
 document.getElementById('helpline-service-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('helpline-service-text').innerText
     const serviceNumber=document.getElementById('helpline-service-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+     calingHotline(serviceText,serviceNumber)
+     
 })
 document.getElementById('electricity-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('electricity-text').innerText
     const serviceNumber=document.getElementById('electricity-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+   calingHotline(serviceText,serviceNumber)
+     
 })
 document.getElementById('brac-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('brac-text').innerText
     const serviceNumber=document.getElementById('brac-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+     calingHotline(serviceText,serviceNumber)
+     
 })
 document.getElementById('railway-call').addEventListener('click',function(){
     
     const serviceText=document.getElementById('railway-text').innerText
     const serviceNumber=document.getElementById('railway-num').innerText
-    const servicetextfinal=serviceText+" "+serviceNumber
-    calingHotline(servicetextfinal)
+     calingHotline(serviceText,serviceNumber)
+     
+})
+
+// clear button task
+
+document.getElementById('clear-button').addEventListener('click',function(){
+const historyDiv=document.getElementById('history-content')
+historyDiv.innerHTML='';
 })
